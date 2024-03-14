456b9b3e2384cee1e9536c1a7ee6a8f04028ae0f
Josef Vyskoèil <josef.vyskocil@cleverlance.com>
2024-03-14 17:49:05 +0100

    version 1.2

--- src/main/java/com/ashkiano/timedrewards/TimedRewards.java
+++ src/main/java/com/ashkiano/timedrewards/TimedRewards.java
@@ -9,6 +9,7 @@ public final class TimedRewards extends JavaPlugin {
         saveDefaultConfig();
         getCommand("reward").setExecutor(new RewardCommandExecutor(this)); // Příkaz "reward" můžete změnit podle vašich preferencí
         Metrics metrics = new Metrics(this, 21241);
+        this.getLogger().info("Thank you for using the TimedRewards plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
     }
 
     @Override
